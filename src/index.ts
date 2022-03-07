cb3363cf5dda6a65f529a6c76058794cc0b29cab
Tomio <mail@tomio.codes>
2022-03-07 10:24:20 +0800

    chore(client): change color to blue

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ process.env.NODE_ENV ??= 'development';
 import '@sapphire/plugin-logger/register';
 import 'reflect-metadata';
 import { DilucClient } from '#root/lib/structures/DilucClient';
-import { green } from 'colorette';
+import { blue } from 'colorette';
 
 const client = new DilucClient();
 
 await client.login();
-client.logger.info(`${green('WS')} - Successfully logged in.`);
+client.logger.info(`${blue('WS')} - Successfully logged in.`);
