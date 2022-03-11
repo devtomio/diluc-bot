53ef1f46ff979b7a4b94732dd19ab46d19d2ebd2
Tomio <mail@tomio.codes>
2022-03-11 14:01:17 +0800

    fix(tag): not editing

--- src/commands/tag.ts
+++ src/commands/tag.ts
@@ -136,7 +136,7 @@ export class SlashCommand extends DilucCommand {
 				'ownerId',
 				interaction.user.id,
 				'ownerTag',
-				`${interaction.user.username}#${interaction.user.tag}`,
+				interaction.user.tag,
 				'createdAt',
 				Date.now()
 			)
@@ -194,12 +194,14 @@ export class SlashCommand extends DilucCommand {
 		const content = submittedModal.fields.getTextInputValue(`content-${interaction.id}`);
 		const ownerId = await this.container.redis.hget(`tags:${name}:${interaction.guildId}`, 'ownerId');
 
-		if (!ownerId) return interaction.editReply("Sorry, that tag doesn't exist.");
+		console.log(msg);
+
+		if (!ownerId) return interaction.reply("Sorry, that tag doesn't exist.");
 
 		const owner = await interaction.guild!.members.fetch(ownerId);
 
 		if (owner.id !== ownerId || cast<GuildMember>(interaction.member).permissions.has(Permissions.FLAGS.MODERATE_MEMBERS))
-			return interaction.editReply("You don't have permissions to edit this tag.");
+			return interaction.reply("You don't have permissions to edit this tag.");
 
 		await this.container.redis
 			.multi()
--- src/lib/util/useModal.ts
+++ src/lib/util/useModal.ts
@@ -1,6 +1,11 @@
-import type { Modal, CommandInteraction } from 'discord.js';
+import type { Modal, CommandInteraction, ModalSubmitInteraction } from 'discord.js';
 
-export const useModal = async (interaction: CommandInteraction, modal: Modal, id: string, timeout = 120_000) => {
+export const useModal = async (
+	interaction: CommandInteraction,
+	modal: Modal,
+	id: string,
+	timeout = 120_000
+): Promise<ModalSubmitInteraction | null> => {
 	await interaction.showModal(modal);
 
 	return interaction.awaitModalSubmit({ time: timeout, filter: (i) => i.customId === `modal-${id}` }).catch(() => null);
